da464fa8b5a2cc5f4dfaa50fc97cfa90838d00c2
Volodymyr Ok <v.shark.t@gmail.com>
2022-12-19 12:07:13 +0200

    added persistor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Navigation from './Navigation/Navigation';
 import Contacts from 'pages/Contacts/Contacts';
 import RegisterView from 'pages/RegisterView/RegisterView';
 import Login from 'pages/LoginView/LoginView';
-import { lazy, Suspense } from 'react';
+// import { lazy, Suspense } from 'react';
 // const Contacts = lazy(() => import('pages/Contacts/Contacts'));
 // const Register = lazy(() => import('pages/Register/Register'));
 // const Login = lazy(() => import('pages/Login/Login'));
